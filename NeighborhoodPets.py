91e2846c0d6cf57c3839dc483200709840567de3
Dennis <lamden@oregonstate.edu>
2022-10-25 09:27:49 -0600

    fixed __init__ format

--- NeighborhoodPets.py
+++ NeighborhoodPets.py
@@ -14,42 +14,42 @@ class DuplicateNameError(Exception):
 class NeighborhoodPets:
     """Class that represents Neigborhood pets"""
 
-    def __init(self):
-        self.pet_dict = {}
+    def __init__(self):
+        self._pet_dict = {}
 
     def add_pet(self, name, species, owner_name):
         """
         Add pet method that adds pet to the pet_dictionary set including the name of the pet, species and the owner's
         name.
         """
-        if name in self.pet_dict:
+        if name in self._pet_dict:
             raise DuplicateNameError
+
         else:
-            self.pet_dict[name] = {'name': name, 'species': species, 'owner name': owner_name}
+            self._pet_dict[name] = {'name': name, 'species': species, 'owner name': owner_name}
 
     def delete_pet(self, name):
         """Delete pet method that deletes the pet from pet_dictionary."""
-        if name in self.pet_dict:
-            del self.pet_dict[name]
+        if name in self._pet_dict:
+            del self._pet_dict[name]
 
     def get_owner(self, name):
         """Get method that returns the name of the owner from pet name in pet_dictionary"""
-        if name in self.pet_dict:
-            return self.pet_dict[name]['owner name']
-        if name not in self.pet_dict:
+        if name in self._pet_dict:
+            return self._pet_dict[name]['owner name']
+        if name not in self._pet_dict:
             pass
 
-    def save_as_json(self, file_name):
+    def save_as_json(self, json_file):
         """Save method that saves file as .json"""
-        with open(file_name, 'w') as outfile:
-            json.dump(self.pet_dict, outfile)
+        with open(json_file, 'w') as outfile:
+            json.dump(self._pet_dict, outfile)
 
-    def read_json(self, file_name):
+    def read_json(self, json_file):
         """Reads .json file"""
-
-        with open(file_name, 'r') as infile:
-            self.pet_dict = json.load(infile)
+        with open(json_file, 'r') as infile:
+            self._pet_dict = json.load(infile)
 
     def get_all_species(self):
         """Get method which returns all the species of the pets"""
-        return self.pet_dict.get('species')
+        return self._pet_dict.get('species')
